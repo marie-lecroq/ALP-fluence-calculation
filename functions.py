ef1f52bde3e0de56de4377cbdf6208c8048c0742
Sebastian Hoof <s.hoof15@imperial.ac.uk>
2019-12-10 12:08:37 +0100

    Further simplifcations to the code.

--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@ from scipy.integrate import quad
 from scipy.interpolate import interp1d
 import numpy.random as rd
 
-
 ### Experimental and physical constants ###
 
 alpha = 1.0/137.035999084 #Fine structure constant
@@ -16,7 +15,7 @@ C = 2.54e71 #Multiplicative constant in (eV^-1)
 T = 30.6e6 #Effective temperature (in eV)
 
 dt = 223.0 #Duration of the measurement (in s)
-min_erg, max_erg = 0.0, 5.0e8 #Min. and max. values of the energy (for integrating the spectrum; in eV)
+min_erg, max_erg = 0.0, 5.0e9 #Min. and max. values of the energy (for integrating the spectrum; in eV)
 erg_window_lo, erg_window_up = 2.5e7, 1.0e8 #Min. and max. values of the energy (the experimental window; in eV)
 number_alps = int(1e7) #Number of particles of fixed m and g in the simulation
 # N.B.: according to arXiv:1702.02964, results are stable for number_alps >= 1e7.
@@ -58,7 +57,7 @@ def sigma(m,g,E):
 def spectrum(m,g,E):
     res = 0.0
     if E > 0:
-        res = C*E*E*sigma(m,g,E)/(np.exp(E/T) - 1.0)
+        res = C*E*E*sigma(m,g,E)/np.expm1(E/T)
     return res
 
 # Axion fluence from SN1987A (= naive fluence for one photon per axion) in (cm^-2)
@@ -71,12 +70,7 @@ erg_vals = np.linspace(min_erg, max_erg, num=5001)
 cdf_vals = []
 # N.B. It is okay to fix the coupling g to a convenient value as it just rescales the spectrum.
 norm = quad(lambda E : spectrum(0.0,1.0e-20,E), min_erg, max_erg)[0]
-for erg in erg_vals:
-    temp = quad(lambda E : spectrum(0.0,1.0e-20,E), min_erg, erg)[0]
-    if norm > 0 :
-        cdf_vals.append(temp/norm)
-    else :
-        cdf_vals.append(0.0)
+cdf_vals = [quad(lambda E : spectrum(0.0,1.0e-20,E)/norm, min_erg, erg)[0] for erg in erg_vals]
 inv_cdf_massless = interp1d(cdf_vals, erg_vals, kind='linear')
 
 
@@ -88,15 +82,16 @@ def expected_photon_fluence(m, g, verbose=0):
     inv_cdf = inv_cdf_massless
     # Only recalculate CDF from spectrum for heavier axions; otherwise the m = 0 version above is used.
     if (m > 1.0e6):
-        cdf_vals = []
-        new_max_erg = min(500.0*m,1.5e9)
+        new_max_erg = min(500.0*m,5.0e9)
+        # Rescale the integral for numerical stability.
+        # N.B. It is okay to fix the value of g to a convenient value as it just rescales the spectrum.
+        erg_pivot = max(1.0e8,5.0*m)
+        int_rescaling = spectrum(m,1.0e-20,erg_pivot)
         n = int((new_max_erg-m)/(0.1*m))+1
         erg_vals = np.linspace(m, new_max_erg, num=n)
-        #  N.B. It is okay to fix the value of g as it just rescales the spectrum.
-        norm = quad(lambda E : spectrum(m,1.0e-20,E), m, new_max_erg)[0]
-        for erg in erg_vals:
-            temp = quad(lambda E : spectrum(m,1.0e-20,E), m, erg)[0]
-            cdf_vals.append(temp/norm)
+        norm = quad(lambda E : spectrum(m,1.0e-20,E)/int_rescaling, m, new_max_erg)[0]
+        norm = norm*int_rescaling
+        cdf_vals = [quad(lambda E : spectrum(m,1.0e-20,E)/norm, m, erg)[0] for erg in erg_vals]
         inv_cdf = interp1d(cdf_vals, erg_vals, kind='linear')
 
     counts = 0
@@ -104,38 +99,38 @@ def expected_photon_fluence(m, g, verbose=0):
     # Generate random numbers from [0,1] and transform to random energies via the inverse CDF.
     rng_ergs = rd.random_sample(number_alps)
     rng_ergs = inv_cdf(rng_ergs)
+    # Consider only energies where an ALP of mass can be produced (should be true for all).
+    rng_ergs = rng_ergs[rng_ergs > m]
 
-    for i in range(number_alps):
-        E = rng_ergs[i]
-        if (E > m):
-            l = l_ALP(m,g,E)
-            L1 = rd.exponential(scale=l, size=None)
-            if (L1 < d) and (L1 > Reff):
-               bf = betafactor(m,E)
-               # Draw random angles in the axion rest frame;
-               # cos(angle) from [-1,1] (i.e. angle in [0,pi]) and phi from [0,2pi]:
-               phi = 2.0*np.pi*rd.random_sample()
-               cos_theta = 2.0*rd.random_sample() - 1.0
-               sin_theta = np.sin(np.arccos(cos_theta))
-               cos_phi = np.cos(phi)
-               # Photon 1. Transform angle and energy into the lab frame:
-               E1 = 0.5*E*(1.0 + bf*sin_theta*cos_phi)
-               if (E1 > erg_window_lo) and (E1 < erg_window_up):
-                   angle1 = np.arccos((bf + cos_phi*sin_theta)/np.abs(1.0 + bf*cos_phi*sin_theta))
-                   y1 =  L1*np.sin(angle1)
-                   L21 = -L1*np.cos(angle1) + np.sqrt(d*d - y1*y1)
-                   time1 = (L1/bf + L21 - d)/c
-                   if (time1 < dt) and (time1 > 0):
-                       counts += 1
-               # Photon 2. Transform angle and energy into the lab frame:
-               E2 = 0.5*E*(1.0 - bf*sin_theta*cos_phi)
-               if (E2 > erg_window_lo) and (E2 < erg_window_up):
-                   angle2 = np.arccos((bf + cos_phi*sin_theta)/np.abs(1.0 + bf*cos_phi*sin_theta))
-                   y2 =  L1*np.sin(angle2)
-                   L22 = -L1*np.cos(angle2) + np.sqrt(d*d - y2*y2)
-                   time2 = (L1/bf + L22 - d)/c
-                   if (time2 < dt) and (time2 > 0):
-                        counts += 1
+    for E in rng_ergs:
+        l = l_ALP(m,g,E)
+        L1 = rd.exponential(scale=l, size=None)
+        if (L1 < d) and (L1 > Reff):
+           bf = betafactor(m,E)
+           # Draw random angles in the axion rest frame;
+           # cos(angle) from [-1,1] (i.e. angle in [0,pi]) and phi from [0,2pi]:
+           phi = 2.0*np.pi*rd.random_sample()
+           cos_theta = 2.0*rd.random_sample() - 1.0
+           sin_theta = np.sin(np.arccos(cos_theta))
+           cos_phi = np.cos(phi)
+           # Photon 1. Transform angle and energy into the lab frame:
+           E1 = 0.5*E*(1.0 + bf*sin_theta*cos_phi)
+           if (E1 > erg_window_lo) and (E1 < erg_window_up):
+               angle1 = np.arccos((bf + cos_phi*sin_theta)/np.abs(1.0 + bf*cos_phi*sin_theta))
+               y1 =  L1*np.sin(angle1)
+               L21 = -L1*np.cos(angle1) + np.sqrt(d*d - y1*y1)
+               time1 = (L1/bf + L21 - d)/c
+               if (time1 < dt) and (time1 > 0):
+                   counts += 1
+           # Photon 2. Transform angle and energy into the lab frame:
+           E2 = 0.5*E*(1.0 - bf*sin_theta*cos_phi)
+           if (E2 > erg_window_lo) and (E2 < erg_window_up):
+               angle2 = np.arccos((bf + cos_phi*sin_theta)/np.abs(1.0 + bf*cos_phi*sin_theta))
+               y2 =  L1*np.sin(angle2)
+               L22 = -L1*np.cos(angle2) + np.sqrt(d*d - y2*y2)
+               time2 = (L1/bf + L22 - d)/c
+               if (time2 < dt) and (time2 > 0):
+                    counts += 1
     lgm, lgg = np.log10(m), np.log10(g)+9.0
     res = naive_one_photon_fluence*counts/float(number_alps)
     if (verbose > 0):
